7b686fba523e6fb41b5be9ee7eae1dd069ced0b4
Michael Gillett <michael.gillett@pacificoffice.com>
2022-12-12 15:37:32 -0800

    Stuff

---
+++ app/static/MOCK_DATA.sql
@@ -0,0 +1,37 @@
+create table MOCK_DATA (
+	id INT,
+	activity_name VARCHAR(50),
+	location VARCHAR(50),
+	category VARCHAR(5),
+	price INT
+);
+insert into MOCK_DATA (id, activity_name, location, category, price) values (1, 'Flipopia', 'Chak Two Hundred Forty-Nine TDA', 'Local', 0);
+insert into MOCK_DATA (id, activity_name, location, category, price) values (2, 'Gigazoom', 'Panambi', 'Major', 0);
+insert into MOCK_DATA (id, activity_name, location, category, price) values (3, 'Topicshots', 'Hatsukaichi', 'Minor', 20);
+insert into MOCK_DATA (id, activity_name, location, category, price) values (4, 'Ooba', 'Koutsopódi', 'Minor', 0);
+insert into MOCK_DATA (id, activity_name, location, category, price) values (5, 'Snaptags', 'Mosteirô', 'Minor', 100);
+insert into MOCK_DATA (id, activity_name, location, category, price) values (6, 'Twinder', 'Yangjiapo', 'Local', 0);
+insert into MOCK_DATA (id, activity_name, location, category, price) values (7, 'Jabbersphere', 'Bang Krathum', 'Minor', 20);
+insert into MOCK_DATA (id, activity_name, location, category, price) values (8, 'Gigazoom', 'Huilong', 'Local', 20);
+insert into MOCK_DATA (id, activity_name, location, category, price) values (9, 'Meezzy', 'Néa Ionía', 'Major', 0);
+insert into MOCK_DATA (id, activity_name, location, category, price) values (10, 'Bubblebox', 'Damiao', 'Local', 20);
+insert into MOCK_DATA (id, activity_name, location, category, price) values (11, 'Cogibox', 'Chaupimarca', 'Local', 100);
+insert into MOCK_DATA (id, activity_name, location, category, price) values (12, 'Twitterbridge', 'Portela', 'Local', 20);
+insert into MOCK_DATA (id, activity_name, location, category, price) values (13, 'Npath', 'Anjozorobe', 'Major', 100);
+insert into MOCK_DATA (id, activity_name, location, category, price) values (14, 'Flipstorm', 'Luodong', 'Major', 0);
+insert into MOCK_DATA (id, activity_name, location, category, price) values (15, 'Rhybox', 'Wanquan', 'Minor', 20);
+insert into MOCK_DATA (id, activity_name, location, category, price) values (16, 'Blogspan', 'Pegões', 'Major', 20);
+insert into MOCK_DATA (id, activity_name, location, category, price) values (17, 'Skinder', 'El Obeid', 'Local', 0);
+insert into MOCK_DATA (id, activity_name, location, category, price) values (18, 'Yodel', 'Huanan', 'Minor', 100);
+insert into MOCK_DATA (id, activity_name, location, category, price) values (19, 'Aimbu', 'Mpophomeni', 'Major', 20);
+insert into MOCK_DATA (id, activity_name, location, category, price) values (20, 'Roodel', 'Xiangshui', 'Local', 100);
+insert into MOCK_DATA (id, activity_name, location, category, price) values (21, 'Flashdog', 'Verin Artashat', 'Local', 0);
+insert into MOCK_DATA (id, activity_name, location, category, price) values (22, 'Linklinks', 'Andekantor', 'Major', 100);
+insert into MOCK_DATA (id, activity_name, location, category, price) values (23, 'Divape', 'Yuguan', 'Minor', 0);
+insert into MOCK_DATA (id, activity_name, location, category, price) values (24, 'Skimia', 'Meirinhas', 'Minor', 0);
+insert into MOCK_DATA (id, activity_name, location, category, price) values (25, 'Youfeed', '‘Alī al Gharbī', 'Minor', 0);
+insert into MOCK_DATA (id, activity_name, location, category, price) values (26, 'Jetpulse', 'Pakokku', 'Local', 100);
+insert into MOCK_DATA (id, activity_name, location, category, price) values (27, 'Skyvu', 'Vavoua', 'Local', 20);
+insert into MOCK_DATA (id, activity_name, location, category, price) values (28, 'Camido', 'Quxi', 'Minor', 20);
+insert into MOCK_DATA (id, activity_name, location, category, price) values (29, 'Lajo', 'Nizwá', 'Local', 0);
+insert into MOCK_DATA (id, activity_name, location, category, price) values (30, 'Yadel', 'Bascaron', 'Minor', 0);
