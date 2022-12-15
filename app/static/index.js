169a838cc7905b8799574895d24f17c5be5a6635
Michael Gillett <michael.gillett@pacificoffice.com>
2022-12-14 16:55:11 -0800

    Stuff

--- app/static/index.js
+++ app/static/index.js
@@ -1,13 +1,3 @@
-// console.log(data[0]['activity_name'])
-
-//let block = document.createElement('div');
-
-//block.setAttribute('class', 'data_node')
-
-//dataBlock.appendChild(block);
-
-//dataBlock.innerHTML += "<h3>This is the text which has been inserted by JS</h3>";
-
 function quickSearch() {
     let input, filter, events, h, text, others, otherText, show;
     input = document.getElementById('quickSearch');
@@ -23,20 +13,14 @@ function quickSearch() {
 
         text = h.textContent || h.innerText;
         if (text.toUpperCase().indexOf(filter) > -1) {
-            // events[i].style.display = "";
             show = true;
-        } else {
-            // events[i].style.display = "none";
         }
 
         for (let j = 0; j < 3; j++) {
             otherText = others[j].textContent || others[j].innerText;
             console.log(otherText);
             if (otherText.toUpperCase().indexOf(filter) > -1) {
-                // events[i].style.display = "";
                 show = true;
-            } else {
-                // events[i].style.display = "none";
             }
         }
 
@@ -49,19 +33,17 @@ function quickSearch() {
     }
 }
 
-function mainSearch() {
-    let name, location, disatnce, category, price, h, others, show, text;
-    name = document.forms["main-form"]["activity"].value ?? '';
-    location = document.forms["main-form"]["location"].value ?? '';
-    distance = document.forms["main-form"]["withinRange"].value ?? '';
-    category = document.forms["main-form"]["category"].value ?? '';
-    price = document.forms["main-form"]["price"].value ?? '';
+function advancedSearch() {
+    let name, location, distance, localCheckbox, minorCheckbox, majorCheckbox;
+    name = document.getElementById('activity').value ?? '';
+    location = document.getElementById('location').value ?? '';
+    distance = document.getElementById('withinRange').value ?? '';
+    localCheckbox = document.getElementById('localCheckbox').checked ?? false;
+    minorCheckbox = document.getElementById('minorCheckbox').checked ?? false;
+    majorCheckbox = document.getElementById('majorCheckbox').checked ?? false;
+    price = document.getElementById('price').value ?? '';
 
     events = document.getElementsByClassName('event');
 
-    for (const i in events) {
-        console.log(i);
-    }
-
-    return false;
+    console.log(`${name}, ${location}, ${distance}, ${localCheckbox}, ${minorCheckbox}, ${majorCheckbox}, ${price}`);
 }
