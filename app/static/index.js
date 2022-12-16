d1fd599ecb6bd5adcc64c56088e853fc7f636806
Michael Gillett <michael.gillett@pacificoffice.com>
2022-12-15 16:02:34 -0800

    Getting there

--- app/static/index.js
+++ app/static/index.js
@@ -6,10 +6,8 @@ function quickSearch() {
 
     for (const i in events) {
         show = false;
-        h = events[i].getElementsByTagName("h1")[0];
+        h = events[i].getElementsByTagName("h2")[0];
         others = events[i].getElementsByTagName("b");
-
-        console.log(others);
 
         text = h.textContent || h.innerText;
         if (text.toUpperCase().indexOf(filter) > -1) {
@@ -18,7 +16,6 @@ function quickSearch() {
 
         for (let j = 0; j < 3; j++) {
             otherText = others[j].textContent || others[j].innerText;
-            console.log(otherText);
             if (otherText.toUpperCase().indexOf(filter) > -1) {
                 show = true;
             }
@@ -34,7 +31,7 @@ function quickSearch() {
 }
 
 function advancedSearch() {
-    let name, location, distance, localCheckbox, minorCheckbox, majorCheckbox;
+    let events, name, location, distance, localCheckbox, minorCheckbox, majorCheckbox, show, h, others, text;
     name = document.getElementById('activity').value ?? '';
     location = document.getElementById('location').value ?? '';
     distance = document.getElementById('withinRange').value ?? '';
@@ -45,5 +42,72 @@ function advancedSearch() {
 
     events = document.getElementsByClassName('event');
 
-    console.log(`${name}, ${location}, ${distance}, ${localCheckbox}, ${minorCheckbox}, ${majorCheckbox}, ${price}`);
+    for (const i in events) {
+        show = true;
+
+        h = events[i].getElementsByTagName("h2")[0];
+        others = events[i].getElementsByTagName("b");
+
+        // activity name
+        if (name) {
+            text = h.textContent || h.innerText;
+            if (!(text.toUpperCase().indexOf(name.toUpperCase()) > -1)) {
+                show = false;
+            }
+        }
+
+        // location
+        if (show && location) {
+            text = others[0].textContent || others[0].innerText;
+            if (!(text.toUpperCase().indexOf(location.toUpperCase()) > -1)) {
+                show = false;
+            }
+        }
+
+        // checkboxes
+        if (show && (localCheckbox || minorCheckbox || majorCheckbox)) {
+            //todo
+            const checkboxes = [];
+
+            if (localCheckbox) {
+                checkboxes.push('Local');
+            }
+
+            if (minorCheckbox) {
+                checkboxes.push('Minor');
+            }
+
+            if (majorCheckbox) {
+                checkboxes.push('Major');
+            }
+
+            text = others[1].textContent || others[1].innerText;
+            if (!checkboxes.includes(text)) {
+                show = false;
+            }
+        }
+
+        // price
+        if (show && price) {
+            text = others[2].textContent || others[0].innerText;
+            if (price < parseInt(text)) {
+                show = false;
+            }
+        }
+
+        if (show) {
+            events[i].style.display = "";
+        }
+        else {
+            events[i].style.display = "none";
+        }
+    }
+}
+
+function clearFields() {
+    let events = document.getElementsByClassName('event');
+
+    for (const i in events) {
+        events[i].style.display = "";
+    }
 }
