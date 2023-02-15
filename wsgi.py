ec3065f8f6608ac4966feda0baebcadd0bc4e9b3
Michael Gillett <michael.gillett@pacificoffice.com>
2023-02-15 09:42:23 -0800

    Stuff

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,11 @@
 import sys
 from app import app
 from flask_frozen import Freezer
+from flask_talisman import Talisman
 
 freezer = Freezer(app)
+
+Talisman(app, content_security_policy=None)
  
 if __name__ == "__main__":
 	if len(sys.argv) > 1 and sys.argv[1] == "build":
