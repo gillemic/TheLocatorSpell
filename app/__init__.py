1b52d55603a4e9f2d7515868881731eacf274a1c
Michael Gillett <michaelgillett19@gmail.com>
2023-02-19 15:36:38 -0800

    stuff

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,15 @@
 from flask import Flask
 from config import Config
 from flask_bootstrap import Bootstrap
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
 from flask_talisman import Talisman
 
-db = SQLAlchemy()
+# db = SQLAlchemy()
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-db.init_app(app)
+# db.init_app(app)
 
 bootstrap = Bootstrap(app)
 
