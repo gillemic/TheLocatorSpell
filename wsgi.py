cd4ec2fc6f99fc1b1a0b9d402f0f26734272b392
Michael Gillett <michael.gillett@pacificoffice.com>
2023-03-14 16:54:35 -0700

    db and login page

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,2 @@
-import sys
-from app import create_app
-from flask_talisman import Talisman
-
-app = create_app()
-
-Talisman(app, content_security_policy=None)
- 
-if __name__ == "__main__":
-	app.run(port=8000)
+from app import create_app as application
+app = application
